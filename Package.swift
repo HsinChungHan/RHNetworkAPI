b6efff59cdd5762039de29fe373315708b45beea
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-01-24 11:24:28 +0800

    Add `RHNetwork` dependency

--- Package.swift
+++ Package.swift
@@ -5,17 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "RHNetworkAPI",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RHNetworkAPI",
             targets: ["RHNetworkAPI"]),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/HsinChungHan/RHNetwork.git",
+            branch: "main")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RHNetworkAPI"),
+            name: "RHNetworkAPI",
+            dependencies: ["RHNetwork"]),
         .testTarget(
             name: "RHNetworkAPITests",
             dependencies: ["RHNetworkAPI"]),
