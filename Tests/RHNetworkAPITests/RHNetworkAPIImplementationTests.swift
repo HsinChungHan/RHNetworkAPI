86f3536db24fdf4a5a0a31417fc4c5b6dcf74909
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-01-25 01:39:48 +0800

    Test `get` and `post` on successful cases

--- Tests/RHNetworkAPITests/RHNetworkAPIImplementationTests.swift
+++ Tests/RHNetworkAPITests/RHNetworkAPIImplementationTests.swift
@@ -1,3 +1,58 @@
+//
+//  RHNetworkAPIImplementationTests.swift
+//
+//
+//  Created by Chung Han Hsin on 2024/1/24.
+//
+
+import XCTest
+@testable import RHNetwork
+@testable import RHNetworkAPI
+
+class RHNetworkAPIImplementationTests: XCTestCase {
+    func test_get_onSuccessfulResult() {
+        let anyData = anyData
+        let anyHttpURLResponse = anyHttpURLResponse
+        let (sut, client) = makeSUT()
+        let exp = expectation(description: "Wait for completion ...")
+        let expectedResult = HTTPClientResult.success(anyData, anyHttpURLResponse)
+        sut.get(path: anyPath) { result in
+            switch (result, expectedResult) {
+            case let (.success(data, response), .success(expectedData, expectedResponse)):
+                XCTAssertEqual(data, expectedData)
+                XCTAssertEqual(response.statusCode, expectedResponse.statusCode)
+                XCTAssertEqual(response.url, expectedResponse.url)
+            default:
+                XCTFail("Expected result \(expectedResult) got \(result) insteasd")
+            }
+            exp.fulfill()
+        }
+        XCTAssertEqual(client.messages[0].request.method, .get)
+        client.complete(with: anyHttpURLResponse.statusCode, data: anyData)
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_post_onSuccessfulResult() {
+        let anyData = anyData
+        let anyHttpURLResponse = anyHttpURLResponse
+        let (sut, client) = makeSUT()
+        let exp = expectation(description: "Wait for completion ...")
+        let expectedResult = HTTPClientResult.success(anyData, anyHttpURLResponse)
+        sut.post(path: anyPath, body: nil) { result in
+            switch (result, expectedResult) {
+            case let (.success(data, response), .success(expectedData, expectedResponse)):
+                XCTAssertEqual(data, expectedData)
+                XCTAssertEqual(response.statusCode, expectedResponse.statusCode)
+                XCTAssertEqual(response.url, expectedResponse.url)
+            default:
+                XCTFail("Expected result \(expectedResult) got \(result) insteasd")
+            }
+            exp.fulfill()
+        }
+        XCTAssertEqual(client.messages[0].request.method, .post)
+        client.complete(with: anyHttpURLResponse.statusCode, data: anyData)
+        wait(for: [exp], timeout: 1.0)
+    }
 private extension RHNetworkAPIImplementationTests {
     var anyData: Data { .init("any data".utf8) }
     var anyURL: URL { .init(string: "http://any-url.com")! }
