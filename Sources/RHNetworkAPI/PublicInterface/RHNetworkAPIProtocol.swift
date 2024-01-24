ccba50f50c09d9c2324b8128441af15ddcbc033a
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-01-24 12:12:31 +0800

    Add `RHNetworkAPIProtocol.swift` as public interface

---
+++ Sources/RHNetworkAPI/PublicInterface/RHNetworkAPIProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  RHNetworkAPIProtocol.swift
+//
+//
+//  Created by Chung Han Hsin on 2024/1/24.
+//
+
+import Foundation
+import RHNetwork
+
+public protocol RHNetworkAPIProtocol {
+    var domain: URL { get }
+    func get(path: String, completion: @escaping (HTTPClientResult) -> Void)
+    func post(path: String, body: [String:String]?, completion: @escaping (HTTPClientResult) -> Void) throws
+}
